2263cc5f4ff2d7e6ba959a1e2760d2bcd33072d2
Michael YVARS <michaelyvars1@gmail.com>
2022-08-21 14:08:14 +0200

    create projects database

---
+++ src/data/projectsData.js
@@ -0,0 +1,18 @@
+export const projectsData = {
+    groupomania: {
+        title: 'Groupomania',
+        date: 'Juillet 2022',
+        languages: ['ReactJS', 'Axios', 'NodeJS', 'Express'],
+        imgUrl: './assets/img/projects/groupomania.jpg',
+        imgAlt: "Page d'accueil de Groupomania",
+        infos: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.',
+    },
+    hottakes: {
+        title: 'HotTakes',
+        date: 'Juin 2022',
+        languages: ['NodeJS', 'Express'],
+        imgUrl: './assets/img/projects/hottakes.jpg',
+        imgAlt: "Page d'accueil de HotTakes",
+        infos: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.',
+    },
+}
