2b3c4c171bc5d55b898ec1fc2c06c2d136dd2112
Michael YVARS <michaelyvars1@gmail.com>
2022-08-21 16:27:30 +0200

    create cursor css

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,16 +4,16 @@ function Navigation() {
     return (
         <div className="navigation">
             <ul className="navigation-content">
-                <NavLink to="/" className={(nav) => (nav.isActive ? 'nav-active' : '')}>
+                <NavLink to="/" className={(nav) => (nav.isActive ? 'nav-active hover' : 'hover')}>
                     accueil
                 </NavLink>
-                <NavLink to="/project/groupomania" className={(nav) => (nav.isActive ? 'nav-active' : '')}>
+                <NavLink to="/project/groupomania" className={(nav) => (nav.isActive ? 'nav-active hover' : 'hover')}>
                     groupomania
                 </NavLink>
-                <NavLink to="/project/hot-takes" className={(nav) => (nav.isActive ? 'nav-active' : '')}>
+                <NavLink to="/project/hot-takes" className={(nav) => (nav.isActive ? 'nav-active hover' : 'hover')}>
                     hot takes
                 </NavLink>
-                <NavLink to="/contact" className={(nav) => (nav.isActive ? 'nav-active' : '')}>
+                <NavLink to="/contact" className={(nav) => (nav.isActive ? 'nav-active hover' : 'hover')}>
                     contact
                 </NavLink>
             </ul>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import { ReactElement } from 'react'
+import Cursor from '../components/Cursor'
 import Navigation from '../components/Navigation'
 
 function Home() {
     return (
         <main className="main-content">
+            <Cursor />
             <Navigation />
         </main>
     )
